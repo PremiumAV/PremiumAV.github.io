86164bb929f7d8f4727d45662f3e7f96fd2602ca
PremiumAV <148028413+PremiumAV@users.noreply.github.com>
2023-10-17 12:05:12 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-// Rich Text
-tinymce.init({
-    selector: "#productDescription",
-    height: 300,
-    plugins: "lists link image",
-    toolbar: "undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image",
-    setup: function (editor) {
-        // Set the initial content with the first word in bold
-        editor.on('init', function () {
-            editor.setContent('<strong>FirstWordBold</strong> Rest of the content.');
-        });
-    }
-});
-
-
 urlParams = new URLSearchParams(window.location.search);
 const productTitle = urlParams.get("productTitle");
 const productDescription = urlParams.get("productDescription");
@@ -23,5 +8,18 @@ const descriptionContentDiv = document.getElementById("tinymce")
 const productPriceDiv = document.getElementById("productPrice");
 
 titleDiv.value = productTitle;
-descriptionContentDiv.innerHTML = productDescription;
 productPriceDiv.value = vendor;
+
+// Rich Text
+tinymce.init({
+    selector: "#productDescription",
+    height: 300,
+    plugins: "lists link image",
+    toolbar: "undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image",
+    setup: function (editor) {
+        // Set the initial content with the first word in bold
+        editor.on('init', function () {
+            editor.setContent(productDescription);
+        });
+    }
+});
